eb1f28c375b03922898f91aaecf23cff4101765a
ChipmunkForLove <52595663+ChipmunkForLove@users.noreply.github.com>
2025-02-13 14:17:18 +0000

    Deploying to gh-pages from @ ChipmunkForLove/ChipmunkForLove.github.io@21d8856353c6acd90eebe924923fe6a9613cf305 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -387,7 +387,7 @@ ninja.data = [{
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
           title: 'project 2',
-          description: "a project with a background image and giscus comments",
+          description: "CryptoCurrency brokerage/exchange platform in LINE japan",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
