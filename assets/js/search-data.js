0097c7381984eb8a1fa20b6cb6496826f84118b5
ChipmunkForLove <52595663+ChipmunkForLove@users.noreply.github.com>
2025-02-17 13:26:54 +0000

    Deploying to gh-pages from @ ChipmunkForLove/ChipmunkForLove.github.io@ff58d9aa2d40c91decb1446fa9084263b88e94ff 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -395,9 +395,9 @@ ninja.data = [{
           description: "Digital Item trading platform",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-dapp-portal",
+          title: 'DApp Portal',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-7",
