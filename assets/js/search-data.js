6c4319f2f5a8d35ba0feef2f2b28eedb72b7f316
ChipmunkForLove <52595663+ChipmunkForLove@users.noreply.github.com>
2025-02-18 15:12:27 +0000

    Deploying to gh-pages from @ ChipmunkForLove/ChipmunkForLove.github.io@043d1dc0a33c245b1db014b827291857662dbe73 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -397,7 +397,7 @@ ninja.data = [{
               window.location.href = "/projects/3_project/";
             },},{id: "projects-dapp-portal",
           title: 'DApp Portal',
-          description: "",
+          description: "LINE in-app service to explore diverse range of mini-Dapps (decentralized applications)",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-7",
